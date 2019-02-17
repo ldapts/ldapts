4c21f6c0a83e175b6aa8fb06def1c507ea43ce34
Adrian Plavka <adrian.plavka@gmail.com>
2019-02-17 23:45:14 +0100

    Added new DNBuilder class, which can chain multiple RDNs & build an escaped string representation of DN

--- src/DN.ts
+++ src/DN.ts
@@ -1,85 +1,115 @@
 
 /**
- * DNMap is a list, which holds key & value list pairs of a distinguished name.
- * This object is used to provide escaping mechanism of inputs.
+ * DNMap is a list, which holds key & value list pairs of a relative distinguished name (RDN).
+ * This type is used to provide escaping mechanism of inputs.
  */
-export type DNMap = DNObject[];
-type DNObject = [string, string | number];
+export type DNMap = RDN[];
+type RDN = [string, string | number];
 
 /**
- * EscapeDN will parse an input DNObject, escape user-provided values & return a string representation.
- *
- * RFC defines, that these characters should be escaped:
- *
- * Comma 	                        ,
- * Backslash character 	          \
- * Pound sign (hash sign) 	      #
- * Plus sign 	                    +
- * Less than symbol 	            <
- * Greater than symbol 	          >
- * Semicolon 	                    ;
- * Double quote (quotation mark) 	"
- * Equal sign 	                  =
- * Leading or trailing spaces
- *
- * @param input DN object to be escaped
- * @returns
+ * DNBuilder provides chain building of multiple RDNs.
+ * When this class is built, it returns an escaped string representation of a DN.
  */
-export function escapeDN(input: DNMap): string {
-  const escapedResults = [];
+export class DNBuilder {
+  private readonly map: DNMap;
 
-  for (const object of input) {
-    const key = object[0];
-    const value = object[1];
-
-    if (typeof value === 'number') {
-      escapedResults.push(`${key}=${value}`);
-      continue;
-    }
-
-    let escapedValue = '';
-    for (const inputChar of value) {
-      switch (inputChar) {
-        case '"':
-          escapedValue += '\\22';
-          break;
-        case '#':
-          escapedValue += '\\23';
-          break;
-        case '+':
-          escapedValue += '\\2b';
-          break;
-        case ',':
-          escapedValue += '\\2c';
-          break;
-        case ';':
-          escapedValue += '\\3b';
-          break;
-        case '<':
-          escapedValue += '\\3c';
-          break;
-        case '=':
-          escapedValue += '\\3d';
-          break;
-        case '>':
-          escapedValue += '\\3e';
-          break;
-        case '\\':
-          escapedValue += '\\5c';
-          break;
-        default:
-          escapedValue += inputChar;
-          break;
-      }
-    }
-
-    // Replace existing trailing & leading whitespaces.
-    escapedValue
-      .replace(/^(\u0020)/gm, '\\ ')
-      .replace(/(\u0020)$/gm, '\\ ');
-
-    escapedResults.push(`${key}=${escapedValue}`);
+  constructor(map?: DNMap) {
+    this.map = map ? map : [];
   }
 
-  return escapedResults.join(',');
+  /**
+   * Add an RDN component to the DN map, consisting of key & value pair.
+   * @param key
+   * @param value
+   * @returns DNBuilder
+   */
+  public addRDN(key: string, value: string | number) {
+    this.map.push([key, value]);
+    return this;
+  }
+
+  /**
+   * Build the DN map, returning escaped string representation of the DN.
+   */
+  public build() {
+    return this._escapeDN(this.map);
+  }
+
+  /**
+   * Parse an input DNMap, escape user-provided values & return a string representation.
+   *
+   * RFC defines, that these characters should be escaped:
+   *
+   * Comma 	                        ,
+   * Backslash character 	          \
+   * Pound sign (hash sign) 	      #
+   * Plus sign 	                    +
+   * Less than symbol 	            <
+   * Greater than symbol 	          >
+   * Semicolon 	                    ;
+   * Double quote (quotation mark) 	"
+   * Equal sign 	                  =
+   * Leading or trailing spaces
+   *
+   * @param input DN object to be escaped
+   * @returns Escaped string representation of DN
+   */
+  private _escapeDN(input: DNMap): string {
+    const escapedResults = [];
+
+    for (const object of input) {
+      const key = object[0];
+      const value = object[1];
+
+      if (typeof value === 'number') {
+        escapedResults.push(`${key}=${value}`);
+        continue;
+      }
+
+      let escapedValue = '';
+      for (const inputChar of value) {
+        switch (inputChar) {
+          case '"':
+            escapedValue += '\\22';
+            break;
+          case '#':
+            escapedValue += '\\23';
+            break;
+          case '+':
+            escapedValue += '\\2b';
+            break;
+          case ',':
+            escapedValue += '\\2c';
+            break;
+          case ';':
+            escapedValue += '\\3b';
+            break;
+          case '<':
+            escapedValue += '\\3c';
+            break;
+          case '=':
+            escapedValue += '\\3d';
+            break;
+          case '>':
+            escapedValue += '\\3e';
+            break;
+          case '\\':
+            escapedValue += '\\5c';
+            break;
+          default:
+            escapedValue += inputChar;
+            break;
+        }
+      }
+
+      // Replace existing trailing & leading whitespaces.
+      escapedValue
+        .replace(/^(\u0020)/gm, '\\ ')
+        .replace(/(\u0020)$/gm, '\\ ');
+
+      escapedResults.push(`${key}=${escapedValue}`);
+    }
+
+    return escapedResults.join(',');
+  }
 }
--- tests/DN.tests.ts
+++ tests/DN.tests.ts
@@ -1,29 +1,39 @@
 
-import { DNMap, escapeDN } from '../src/DN';
+import { DNMap, DNBuilder } from '../src/DN';
 
 describe('DN', () => {
-  describe('DNMap', () => {
+  describe('DNBuilder', () => {
+    it('should build & escape correctly', () => {
+      const dn = new DNBuilder()
+        .addRDN('cn', 'Smith, James K.')
+        .addRDN('dc', 'env.organization')
+        .addRDN('dc', 'env.domain')
+        .build();
+
+      dn.should.equal('cn=Smith\\2c James K.,dc=env.organization,dc=env.domain');
+    });
+
     it('should escape correctly', () => {
       let dn: DNMap;
 
       dn = [['cn', 'Smith, James K.']];
-      escapeDN(dn).should.equal('cn=Smith\\2c James K.');
+      new DNBuilder(dn).build().should.equal('cn=Smith\\2c James K.');
 
       dn = [['ou', 'Sales\\Engineering']];
-      escapeDN(dn).should.equal('ou=Sales\\5cEngineering');
+      new DNBuilder(dn).build().should.equal('ou=Sales\\5cEngineering');
 
       dn = [['cn', 'East#Test + Lab']];
-      escapeDN(dn).should.equal('cn=East\\23Test \\2b Lab');
+      new DNBuilder(dn).build().should.equal('cn=East\\23Test \\2b Lab');
     });
 
     it('should escape trailing & leading whitespaces', () => {
       let dn: DNMap;
 
       dn = [['cn', ' Jim Smith ']];
-      escapeDN(dn).should.equal('cn=\ Jim Smith\ ');
+      new DNBuilder(dn).build().should.equal('cn=\ Jim Smith\ ');
 
       dn = [['cn', '  Bob   Barker']];
-      escapeDN(dn).should.equal('cn=\  Bob   Barker');
+      new DNBuilder(dn).build().should.equal('cn=\  Bob   Barker');
     });
   });
 });
