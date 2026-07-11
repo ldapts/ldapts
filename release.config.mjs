0be5821c996d67106de12c1bd3acccf90bf778f6
Jim Geurts <jim@biacreations.com>
2026-07-11 10:38:52 -0500

    Revert "fix: make automated releases work with the protected main branch"
    This reverts commit b04591d38a44d3bbd9ca12711af61910da30e1ed.

--- release.config.mjs
+++ release.config.mjs
@@ -27,11 +27,24 @@ export default {
       },
     ],
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
     [
       '@semantic-release/github',
       {
         assets: [],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: `chore: release \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
       },
     ],
   ],
